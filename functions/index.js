052a3f21521eb6fa3af67590d9bc6464df37af1a
mdanisurrahmanrony <mdanisurrahmanrony49@gmail.com>
2021-01-31 03:09:20 +0600

    Change Stripe Secret Key

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51HPvU9DFg5koCdLGeOEiFvwHat4v8eMjX6SY0YCwxPBQBUPhKy1fPVhiSM5cQtgW7QBG9ydQcXnW57TDxVE2f3H000HSfmEQZF"
+  "sk_test_51IFQWqKSH9oO2jcLYY52nE3BhRUPcjzSMPKDjIK3ZS0qXGEHhGYBGxUu2rgHzp4zRYWeTDAkHdNTqfNcLKGyaqU0004GD8nMFi"
 );
 
 // API
